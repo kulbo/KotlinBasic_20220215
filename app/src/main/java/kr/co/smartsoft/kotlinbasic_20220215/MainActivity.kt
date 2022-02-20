19ade66fb8319ee1d14d571f26b97f1e39e1c27f
이 성 노 <kulbo0926@gmail.com>
2022-02-20 15:20:00 +0900

    버튼이 눌리면 동작하도록 수정

--- app/src/main/java/kr/co/smartsoft/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/kr/co/smartsoft/kotlinbasic_20220215/MainActivity.kt
@@ -2,13 +2,18 @@ package kr.co.smartsoft.kotlinbasic_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnLog : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLog.setOnClickListener { 
+            Log.d("메인화면","로그 버튼 눌림")
+        }
     }
 }
